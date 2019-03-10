0689429f91e5ffc012a2facd5b7be59bd2c3f74a
jarlow <you@example.com>
2019-03-10 12:38:55 +0100

    la till results o testing

--- testing.sh
+++ testing.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-if [ $# -ne 2 ];then
-	echo -e "usage: \"testing.sh (int)forkAfter (int)number_of_testing_iterations\"\nexample usage: \"testing.sh 9 150\" \nmeans forkAfter is 9 and amount of tests is 150"
+if [ $# -ne 3 ];then
+	echo -e "usage: \"testing.sh (int)forkAfter (int)number_of_testing_iterations (string)map\"\nexample usage: \"testing.sh 9 150 maps/medium.map\" \nmeans forkAfter is 9 and amount of tests is 150 on map medium"
 	exit 1
 fi
+FORKAFTER=$1
 AMOUNTOFTESTS=$2
-COUNTER=0
+MAPPATH=$3
+MAPNAME="${MAPPATH#*/}"
+
+TIMEARR=()
+FAILCOUNTER=0
+
 for i in $(seq 1 $AMOUNTOFTESTS);do
-	MSG=$(java -cp src/main amazed.Main maps/medium.map parallel-$1 -1)
+	MSG=$(java -cp src/main amazed.Main $MAPPATH parallel-$FORKAFTER -1)
 	ALLTEXT=$(echo $MSG | grep "Goal found")
 	RESULT=$?
+	TIMEARR+=($(echo $MSG | grep -Eo '[0-9]+'))
 	if [ "$RESULT" -ne 0 ];then
-		let COUNTER=COUNTER+1
+		let FAILCOUNTER=COUNTER+1
 		echo "$i Failure"
 		echo $MSG
 	else 
@@ -18,6 +25,9 @@ for i in $(seq 1 $AMOUNTOFTESTS);do
 		echo $MSG
 	fi
 done
-echo "This many failures out of $AMOUNTOFTESTS :"
-echo $COUNTER
-
+echo ""
+echo "This many failures out of $AMOUNTOFTESTS runs: $FAILCOUNTER"
+echo ""
+printf -v TIMEARR_d ',%s' "${TIMEARR[@]}"
+TIMEARR_d=${TIMEARR_d:1}
+echo $TIMEARR_d | tee results/test_${MAPNAME}_fork${FORKAFTER}_iterations${AMOUNTOFTESTS}.csv
